c7d95fa02ec97c36415ec185fe47a2a6171535dd
alberto-piccina <alberto.piccina@studenti.unipd.it>
2025-03-11 17:43:32 +0100

    NEW - Added methods (add_pokemon, remove_pokemon, check_team) in Trainer class.

--- pokemon_trainer.py
+++ pokemon_trainer.py
@@ -24,3 +24,25 @@ class Trainer:
         count = Cnt(self.items)
         for item, frequency in count.items():
             print("->", item, "x", frequency)
+            
+    # method to add a pokemon in the team
+    def add_pokemon(self, pokemon):
+        if len(self.pokemon_list) < 6:
+            self.pokemon_list.append(pokemon)
+            print(pokemon.name, "has been added to", self.name,"'s team!")
+        else:
+            print(self.name, ", there is no more space in your team!")
+            
+    # method to remove a pokemon to the team
+    def remove_pokemon(self, pokemon):
+        if pokemon in self.pokemon_list:
+            self.pokemon_list.remove(pokemon)
+        else:
+            print(pokemon.name, "is not in", self.name, "'s team!")
+    
+    # method to check the pokemon team
+    def check_team(self):
+        pokemon_number = len(self.pokemon_list)
+        print("\n", self.name, "'s team is composed by", pokemon_number, "Pokemon:\n")
+        for i in self.pokemon_list:
+            print(i.name, "   ", "Lv.", i.level)
